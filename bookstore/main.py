96885af9f5f333ed0a11351ec15675adb8b3de00
TheSarcasticOne69 <shivamkushwaha197@gmail.com>
2025-10-02 17:05:36 +0530

    initial commit

---
+++ bookstore/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import book
+
+app = FastAPI(title="BookStore API")
+
+
+origins = ["https://your-frontend.vercel.app"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(book.router)
